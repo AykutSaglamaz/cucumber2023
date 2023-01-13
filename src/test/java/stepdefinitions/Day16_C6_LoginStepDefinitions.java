c13eaa825665dd0cb7656bedc7ff3274ded45cf9
AykutSaglamaz <gulerrahim@gmail.com>
2023-01-13 05:05:12 -0500

    cucumber3

---
+++ src/test/java/stepdefinitions/Day16_C6_LoginStepDefinitions.java
@@ -0,0 +1,50 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+import pages.DefaultPage;
+import pages.LoginPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class Day16_C6_LoginStepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+    DefaultPage defaultPage = new DefaultPage();
+
+    @Given("kullanici application sayfasindadir")
+    public void kullanici_application_sayfasindadir() {
+        Driver.getDriver().get(ConfigReader.getProperty("app_url_login"));
+        try{
+            Thread.sleep(1000);
+            loginPage.advancedLink.click();
+            Thread.sleep(1000);
+            loginPage.proceedLink.click();
+            Thread.sleep(1000);
+        }catch (Exception e){
+            System.out.println("Advanced Link and Proceed Link is not displayed");
+        }
+
+    }
+    @Given("kullanici manager id {string} girer")
+    public void kullanici_manager_id_girer(String string) {
+        loginPage.username.sendKeys(string);
+
+    }
+    @Given("kullanici manager sifre {string} girer")
+    public void kullanici_manager_sifre_girer(String string) {
+        loginPage.password.sendKeys(string);
+
+    }
+    @Then("kullanici login buttonuna click eder")
+    public void kullanici_login_buttonuna_click_eder() {
+        loginPage.loginButton.click();
+
+    }
+    @Then("kullanici {string} oldugunu very eder")
+    public void kullanici_oldugunu_very_eder(String string) {
+        Assert.assertEquals(defaultPage.hotelManagementLink.getText(), string);
+
+
+    }
+}
---
+++ src/test/resources/Sprint_13.05/Eagles/Day16_C5_US100450_kullanici_manager_kullanici_bilgilerini_kullanarak_login_yapmali.feature
@@ -0,0 +1,15 @@
+@manager_login_test
+  Feature: Login_manager_kullanici_bilgileriyle_deneme
+    Scenario Outline:
+      Given kullanici application sayfasindadir
+      And kullanici manager id "<kullaniciadi>" girer
+      And kullanici manager sifre "<kullanicisifre>" girer
+      Then kullanici login buttonuna click eder
+      Then kullanici "<default_page>" oldugunu very eder
+      Then kullanici application kapatir
+
+      Examples: Test Data
+        | kullaniciadi | kullanicisifre | default_page |
+        | manager      | Manager1!      | Hotel Management |
+        | manager7     | Manager7!      | default_page |
+        | manager12    | Manager12!     | default_page |
